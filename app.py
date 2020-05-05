922229006d29e9d5efebab8309caf0ce307f6ac8
Ryan Keys <r.keys1998@gmail.com>
2020-05-04 22:15:16 -0700

    commit

--- app.py
+++ app.py
@@ -1,38 +1,56 @@
 import os
-source = os.getcwd()
+import sys
+
 all_dirs = []
+debug = True
 
 
-def get_all_dirs(folder):
+def get_all_dirs(active_dir):
     '''
     Retrieves all directories
 
-    Takes the directory PyFileManager is located in unless specified otherwise(takes your path as a string. ex: folder = os.getcwd()) and returns all subdirectories as a list of dictionaries.
+    Takes the directory PyFileManager is located in unless specified otherwise(takes your path as a string. ex: active_dir = os.getcwd()) and returns all subdirectories as a list of dictionaries.
 
     Parameters:
-    folder (list): List of all files in your current directory.
+    active_dir (list): List of all files in your current directory.
 
     Returns:
     list: List of dictionaries containing subdirectory name and the path.
 
+    Returns:
+    bool: If no directories are found returns None instead
+
     '''
-    folders = os.listdir(folder)
-    if len(folders) > 0:
-        for potential_subdir in folders:
+    # creates list of dirs inside specified path
+    subdirs = os.listdir(active_dir)
+    if len(subdirs) > 0:
+        for potential_subdir in subdirs:
+            # ignores files with extensions
             if "." in potential_subdir:
                 pass
 
             else:
-                potential_subdir_path = os.path.join(
-                    folder, potential_subdir)
-                all_dirs.append({potential_subdir: potential_subdir_path})
-                get_all_dirs(potential_subdir_path)
+                # makes new path from found subdirectory
+                subdir_path = os.path.join(
+                    active_dir, potential_subdir)
+                # appends name of directory, and path in key, value pair
+                all_dirs.append({potential_subdir: subdir_path})
+                # looks inside subdirectory for even more directories
+                get_all_dirs(subdir_path)
+    # if no directories are present
+    if len(all_dirs) == 0:
+        return None
     return all_dirs
 
 
 def test():
-    print(get_all_dirs(folder=source))
+    print(get_all_dirs(active_dir=source))
 
 
-if __name__ == "__main__":
+if __name__ == "__main__" and debug == True:
+    source = os.getcwd()
     test()
+
+if __name__ == "__main__" and debug == False:
+    source = sys.argv[1]
+    print(get_all_dirs(source))
